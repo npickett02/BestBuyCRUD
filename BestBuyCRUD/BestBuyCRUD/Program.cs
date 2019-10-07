9252b51e59fe479576206509208fb80529473766
nichols pickett <npickett02@gmail.com>
2019-10-07 14:14:49 -0500

    changes

---
+++ BestBuyCRUD/BestBuyCRUD/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace BestBuyCRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var departments = GetAllDepartments();
+
+            foreach(var department in departments)
+            {
+                Console.Write(department);
+            }
+        }
+
+        static List<string> GetAllDepartments()
+        {
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = System.IO.File.ReadAllText("connectionString.txt");
+
+            MySqlCommand cmd = conn.CreateCommand();
+
+            cmd.CommandText = "SELECT Name FROM Departments;";
+
+
+            using (conn)
+            {
+                conn.Open();
+                List<string> allDepartments = new List<string>();
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read() == true)
+                {
+                    var currentDepartment = reader.GetString("Name");
+                    allDepartments.Add(currentDepartment);
+                }
+
+                return allDepartments;
+
+            }
+        }
+    }
+}
